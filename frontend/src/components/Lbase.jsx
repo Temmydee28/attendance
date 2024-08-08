c4eff54adddd4997664cc70d83f0dbfa3a81d735
Temmydee28 <89853794+Temmydee28@users.noreply.github.com>
2024-08-08 02:59:08 +0100

    Update axios of Lbase.jsx

--- frontend/src/components/Lbase.jsx
+++ frontend/src/components/Lbase.jsx
@@ -18,7 +18,7 @@ const DataTable = () => {
         const fetchUserData = async () => {
             try {
                 const token = localStorage.getItem('token');
-                const response = await axios.get('http://localhost:8001/lrecord', {
+                const response = await axios.get('https://attendance-rose.vercel.app//lrecord', {
                     headers: {
                         Authorization: token,
                     }
